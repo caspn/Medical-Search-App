9cddb8cc2adfbd818787e9f53767b929153e84d1
caspn <scian.stefano.jw@gmail.com>
2026-03-24 21:32:39 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configurazione applicazione — valori offuscati
+// NON modificare manualmente: usa il pannello Admin → Esporta per aggiornare.
+window.__CFG = {
+  A: 'DCIuW15RVVIdCHIFCAoC',
+  P: 'DikuYVFcZG98dHA=',
+  S: 'JTI3QkMIGw4pKSBBHlVbTioqJhxTXVkOPjYxV1FWR0koIzdBH1YbECMkE1d3Z0dmCxw7Q35zUnQYLytcfnxtV3x/EHgFSn4ZKw4gBGcFUEg9JyQdVVZdVXIhKlYNAwUXend3AgcEDQIqLycPAQMCFnxycwUGCw==',
+  D: 'fA=='
+};
