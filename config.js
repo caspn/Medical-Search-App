292dc2e6bd5ee329eee679e92dd9e7be798b508f
caspn <scian.stefano.jw@gmail.com>
2026-04-07 11:02:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// Configurazione applicazione — valori offuscati
-// NON modificare manualmente: usa il pannello Admin → Esporta per aggiornare.
 window.__CFG = {
   A: 'DCIuW15RVVIdCHIFCAoC',
   P: 'DikuYVFcZG98dHA=',
-  S: 'JTI3QkMIGw4pKSBBHlVbTioqJhxTXVkOPjYxV1FWR0koIzdBH1YbECMkE1d3Z0dmCxw7Q35zUnQYLytcfnxtV3x/EHgFSn4ZKw4gBGcFUEg9JyQdVVZdVXIhKlYNAwUXend3AgcEDQIqLycPAQMCFnxycwUGCw==',
+  S: 'JTI3QkMIGw4pKSBBHlVbTioqJhxTXVkOPjYxV1FWR0koIzdBH1YbRGJ0E3NzahkQOxICSAV5dkk+cjt0SmNiVhlzAgICdRlTGRQTZABgAnUCDCwBWGEHZg8iCQd/aGxkLDIqYVNicWsSdAJ/aFFjbjokFlNkfl0XIzUselZHAA49MyFaRF9Y',
   D: 'fA=='
 };
